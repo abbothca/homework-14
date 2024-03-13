ad09e43e1d003abaee6f992edbc09a7fedc3d8e0
Liubov Khomych <kroka.luba@gmail.com>
2024-03-13 22:14:47 +0200

    code from the workshop

---
+++ js/script.js
@@ -0,0 +1,120 @@
+"use strict";
+
+// const
+const TASKS_STORAGE_KEY = "tasks";
+
+// DOM variables
+const form = document.querySelector(".create-task-form");
+const taskInput = document.querySelector(".task-input");
+const taskList = document.querySelector(".collection");
+const clearButton = document.querySelector(".clear-tasks");
+const filterInput = document.querySelector(".filter-input");
+
+// "storage" functions
+const getTasksFromStorage = () => {
+    return JSON.parse(localStorage.getItem(TASKS_STORAGE_KEY)) || [];
+};
+
+const storeTaskInStorage = (newTask) => {
+    const tasks = getTasksFromStorage();
+    tasks.push(newTask);
+
+    localStorage.setItem(TASKS_STORAGE_KEY, JSON.stringify(tasks));
+};
+
+const clearTasksFromStorage = () => {
+    localStorage.removeItem(TASKS_STORAGE_KEY);
+};
+
+const removeTaskFromStorage = (deletedTask) => {
+    const tasks = getTasksFromStorage();
+
+    const deletedIndex = tasks.findIndex((task) => task === deletedTask);
+    tasks.splice(deletedIndex, 1);
+
+    localStorage.setItem(TASKS_STORAGE_KEY, JSON.stringify(tasks));
+};
+
+//
+
+// "tasks" functions
+const appendLi = (value) => {
+    // Create and add LI element
+    const li = document.createElement("li");
+    li.innerHTML = `${value} <i class="fa fa-remove delete-item"></i>`;
+    taskList.append(li);
+};
+
+const addTask = (event) => {
+    event.preventDefault();
+
+    // Перевірка на пусте значення
+    const value = taskInput.value.trim();
+    if (value === "") {
+        return;
+    }
+
+    appendLi(value);
+
+    // Очистити форму
+    taskInput.value = "";
+    // form.reset();
+
+    // Фокусуємось на input
+    taskInput.focus();
+
+    // Зберігаємо елемент у localStorage
+    storeTaskInStorage(value);
+};
+
+const clearTasks = () => {
+    taskList.innerHTML = "";
+    clearTasksFromStorage();
+};
+
+const removeTask = (event) => {
+    const isDeleteButton = event.target.classList.contains("delete-item");
+    if (!isDeleteButton) {
+        return;
+    }
+
+    const isConfirmed = confirm("Ви впевнені що хочете видалити це завдання?");
+    if (!isConfirmed) {
+        return;
+    }
+
+    const li = event.target.closest("li");
+    li.remove();
+
+    // Видалити зі сховища
+    const deletedTask = li.textContent.trim();
+    removeTaskFromStorage(deletedTask);
+};
+
+const filterTasks = ({ target: { value } }) => {
+    const text = value.toLowerCase();
+    const list = taskList.querySelectorAll("li"); // []
+
+    list.forEach((li) => {
+        const liText = li.textContent.trim().toLowerCase();
+
+        li.hidden = !liText.includes(text);
+    });
+};
+
+const initTasks = () => {
+    const tasks = getTasksFromStorage();
+    tasks.forEach(appendLi);
+};
+
+// Init
+initTasks();
+
+// Event listeners
+form.addEventListener("submit", addTask);
+
+clearButton.addEventListener("click", clearTasks);
+
+taskList.addEventListener("click", removeTask);
+
+filterInput.addEventListener("input", filterTasks);
